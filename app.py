8168fa88a6c0603a96a331feec85f0b702d20fc0
Yashraj Dudhe <76808376+yashraj-dudhe@users.noreply.github.com>
2024-07-08 18:29:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from prdct import show_predict_page
+from explore import show_explore_page
+
+
+page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
